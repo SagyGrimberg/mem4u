5d47e70c4000b16c294c3ea7e1f0c4153506da9c
SagyGrimberg <sagythe@gmail.com>
2022-02-16 18:18:53 +0200

    made ad look better

--- frontend/src/components/Ad.js
+++ frontend/src/components/Ad.js
@@ -1,18 +1,13 @@
-import {useState} from "react";
-import {Button, Modal} from "react-bootstrap";
+import {Modal} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
-import {DISMISS_AD} from "../constants/adConstants";
 import {dismissAd} from "../actions/adActions";
 
 const Ad = () => {
     const dispatch = useDispatch();
-    const [show, setShow] = useState(false);
     const ad = useSelector((state) => state.adInfo);
     const handleClose = () => {
         dispatch(dismissAd());
-        setShow(false);
     }
-    const handleShow = () => setShow(true);
 
     return (
 
@@ -23,14 +18,6 @@ const Ad = () => {
             <Modal.Body><img
                 src={ad.image}
                 className="img-fluid" alt="place image here"/></Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
         </Modal>
     );
 }
