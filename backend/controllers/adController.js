8e5a913bdb044fc74724ccb2305dcf223a6a6a17
SagyGrimberg <sagythe@gmail.com>
2022-02-16 16:48:23 +0200

    added controller functions to ads

--- backend/controllers/adController.js
+++ backend/controllers/adController.js
@@ -1,9 +1,56 @@
-/*
+import asyncHandler from "express-async-handler";
+import Ad from "../models/adModel.js";
 
-export const sendRandomAd = async (socket) => {
+export const getAds = asyncHandler(async (req, res) => {
+    const ads = await Ad.find({})
+    res.json(ads);
+})
+export const createAd = asyncHandler(async (req, res) => {
+    const ad = new Ad({
+        name: 'מוצר בעריכה',
+        image: '/images/sample.jpg',
+    })
+    const createdProduct = await ad.save()
+    res.status(201).json(createdProduct)
+})
+export const updateAd = asyncHandler(async (req, res) => {
+    const {
+        name,
+        image,
+    } = req.body
 
-}
-export const sendRandomAdToRoom = async () => {
-    io
-}
-*/
+    const ad = await Ad.findById(req.params.id)
+
+    if (ad) {
+        ad.name = name
+        ad.image = image
+
+        const updateAd = await ad.save()
+
+        res.json(updateAd)
+    } else {
+        res.status(404)
+        throw new Error('המודעה לא נמצא')
+    }
+})
+export const deleteAd = asyncHandler(async (req, res) => {
+    const ad = await Ad.findById(req.params.id)
+
+    if (ad) {
+        await ad.remove()
+        res.json('המודעה הוסר')
+    } else {
+        res.status(404)
+        throw new Error('המודעה לא נמצא')
+    }
+})
+export const getAdById = asyncHandler(async (req, res) => {
+    const ad = await Ad.findById(req.params.id)
+
+    if (ad) {
+        res.json(ad)
+    } else {
+        res.status(404)
+        throw new Error('המודעה לא נמצא')
+    }
+})
