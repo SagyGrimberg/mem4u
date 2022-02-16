71694bd8955745db6679f32e7660308b09e5cacd
Ronen Pinhasov <ronenpi18@gmail.com>
2022-02-16 16:55:56 +0200

    tried to solve agenda bugs

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -8,9 +8,11 @@ const agenda = new Agenda({db: {address: db_url}})
 export const userIdsToSocketId = new Map();
 const DEFAULT_AD_INTERVAL = "30 seconds";
 export const initConnection = () => {
-    _io.on("connection", (socket) => {
+    _io.on("connection", async (socket) => {
+        await agenda.start();
         console.info(`user connected to server!`);
         const sessionId = socket.id;
+
         agenda.define(
             sessionId,
             {priority: 10},
@@ -45,9 +47,10 @@ export const initConnection = () => {
 const updateUserAdTimes = async (userId, socket) => {
     let socketId = socket ? socket.id : userIdsToSocketId.get(userId);
     const user = await User.findById(userId).exec();
+
     if (user?.adOptions?.frequency) {
         await agenda.cancel({name: socketId});
-        await agenda.every(user.adOptions.frequency, socketId);
+        !user.isAdmin && await agenda.every(user.adOptions.frequency, socketId);
     }
 }
 const newAdLoop = async (job) => {
@@ -56,16 +59,7 @@ const newAdLoop = async (job) => {
     let random = Math.floor(Math.random() * count);
     const chosenAd = await Ad.findOne().skip(random).exec();
     console.log(chosenAd)
-    socket.emit(`AdClientUpdates`, chosenAd);
+    chosenAd && socket.emit(`AdClientUpdates`, chosenAd);
 
     console.log(job);
 }
-/*
-const task = new ('name', () => {
-    return async () => {
-        const count = await Ad.count();
-        let random = Math.floor(Math.random() * count);
-        const chosenAd = await Ad.findOne().skip(random).exec();
-
-    }
-})*/
